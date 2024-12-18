5883a8af0aee749f7cf59479a19a8df58014feeb
tuan6100 <tuananhho378@gmail.com>
2024-12-18 18:58:40 +0700

    update webserver

--- server/gradle/src/main/java/com/chat/app/security/MySecretKey.java
+++ server/gradle/src/main/java/com/chat/app/security/MySecretKey.java
@@ -2,7 +2,7 @@ package com.chat.app.security;
 
 public class MySecretKey {
 
-    public final static String JWT_SECRET = "t";
+    public final static String JWT_SECRET = "";
 
     public final static String AWS_ACCESS_KEY_ID = "";
     public final static String AWS_SECRET_ACCESS_KEY = "";

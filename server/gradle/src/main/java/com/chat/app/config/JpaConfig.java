b462bc632a72a7bc2fd65e324064f837d498a7af
tuan6100 <tuananhho378@gmail.com>
2024-12-27 20:52:01 +0700

    delete file

--- server/gradle/src/main/java/com/chat/app/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.chat.app.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(basePackages = {"com.chat.app.repository.jpa", "com.chat.app.security"})
-public class JpaConfig {
-}

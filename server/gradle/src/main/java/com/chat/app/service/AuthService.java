ef62b39d009c3909f99a2dcdd3a0e7ffb2b37f8e
tuan6100 <tuananhho378@gmail.com>
2024-12-31 19:59:53 +0700

    update Exception

--- server/gradle/src/main/java/com/chat/app/service/AuthService.java
+++ server/gradle/src/main/java/com/chat/app/service/AuthService.java
@@ -1,6 +1,6 @@
 package com.chat.app.service;
 
-import com.chat.app.exception.ChatException;
+import com.chat.app.exception.UnauthorizedException;
 import com.chat.app.model.entity.Account;
 import com.chat.app.payload.request.AuthRequestWithEmail;
 import com.chat.app.payload.request.AuthRequestWithUsername;
@@ -18,11 +18,11 @@ public interface AuthService {
 
     public HttpHeaders getResponseHeader(Authentication auth, Account account);
 
-    public AuthResponse login(AuthRequestWithEmail authRequest) throws ChatException;
+    public AuthResponse login(AuthRequestWithEmail authRequest) throws UnauthorizedException;
 
-    public AuthResponse register(Account account) throws ChatException;
+    public AuthResponse register(Account account) throws UnauthorizedException;
 
-    public AuthResponse updatePassword(AuthRequestWithEmail authRequest) throws ChatException;
+    public AuthResponse updatePassword(AuthRequestWithEmail authRequest) throws UnauthorizedException;
 
-    public Account resetPassword(ResetPasswordRequest resetPasswordRequest) throws ChatException;
+    public Account resetPassword(ResetPasswordRequest resetPasswordRequest) throws UnauthorizedException;
 }
